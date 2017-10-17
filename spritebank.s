cbb90ea1aa1a98e354ed0f916318c132c0d8beeb
blondie7575 <qd@quinndunki.com>
2017-10-17 08:21:29 -0700

    Fixed art pipeline bug

--- spritebank.s
+++ spritebank.s
@@ -2,16 +2,16 @@
 
 
 DrawSpriteBank :
- 	 ASL 	  	 ; A=Sprite Number ($0000-$0002)
+ 	 ASL 	  	 ; A=Sprite Number ($0000-$0003)
 	  	 TAX 	  	 ; Y=Target Screen Address ($2000-$9D00)
 	  	 LDA 	 SpriteBankNum,X 	 ; Relative Sprite Number Table
 	  	 JMP 	 (SpriteBankBank,X) 	 ; Bank Number Table
 	  	  	  	 
 SpriteBankNum :
- 	 .dbyt 	 0000,0200,0100 	 
+ 	 .dbyt 	 $0000,$0300,$0200,$0100 	 
 	  	  	  	 
 SpriteBankBank :
- 	 .addr 	 SpriteBankBank00,SpriteBankBank00,SpriteBankBank00 	 
+ 	 .addr 	 SpriteBankBank00,SpriteBankBank00,SpriteBankBank00,SpriteBankBank00 	 
 	  	  	  	 
 SpriteBankBank00 :
  	 JSL 	 $AA0000 	 
